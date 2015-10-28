417b8ac7ab625b64891711c73aed9da7be11ed4c
deki <deki@ap-imac-02.honda-eng.co.jp>
2015-10-28 15:47:43 +0900

    markdown-modeの設定

--- emacs/elisp/load-markdown.el
+++ emacs/elisp/load-markdown.el
@@ -1,3 +1,5 @@
 ;; [2015-10-28]
-;; http://cloverrose.hateblo.jp/entry/2014/11/03/220452
+;; http://masutaka.net/chalow/2012-11-23-1.html
 ;; を参考に環境構築することにする
+(require 'markdown-mode)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
