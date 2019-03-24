1a066e307d3c830243f8f3dc1d125be29389227d
dekigl <dekigl@gmail.com>
2019-03-24 17:46:08 +0900

    新なmajor-modeの設定追加 js2-mode web-mode

---
+++ emacs/inits/04-js2-mode.el
@@ -0,0 +1,5 @@
+(add-hook 'js2-mode-hook
+	  (lambda ()
+	    (setq js2-basic-offset 2)
+	    (setq js-switch-indent-offset 2)
+	    ))
---
+++ emacs/inits/04-web-mode.el
@@ -0,0 +1,3 @@
+(setq auto-mode-alist
+      (append '(("\\.html"  . web-mode)
+		) auto-mode-alist))
